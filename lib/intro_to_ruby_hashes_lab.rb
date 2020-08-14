b8a96afba895d1ae69e0f35be16632c29e619565
confident-architect-8379 <s.kanagui@gmail.com>
2020-08-14 19:14:47 +0000

    Done.

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,16 +1,19 @@
 def new_hash
-  # return an empty hash
+  {}
 end
 
 def my_hash
+  {:name => "John"}
   # return a valid hash with any key/value pair of your choice
 end
 
-def pioneer
-  # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
+def pioneer 
+  {:name => "Grace Hopper"}
+
 end
 
 
 def id_hash_generator(number)
+  {:id => number}
   # return a hash with a key :id assigned to the provided number
 end
